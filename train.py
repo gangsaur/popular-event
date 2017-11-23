78c61dd9d95e7ef0cc1ec1a6c5de30e7197fea6a
gangsaur <andrihardono@yahoo.com>
2017-11-23 20:26:07 +0700

    Use all dataset

--- train.py
+++ train.py
@@ -3,34 +3,38 @@ from time import time
 import sklearn_crfsuite
 import sklearn_crfsuite.metrics
 
-file_name = "kompas.txt"
-f = open ("./data/" + file_name, 'r', encoding="UTF-8")
-i = 0
-
+file_names = ["kompas.txt","detik.txt","tempo.txt","beritasatu.txt","metro.txt"]
 tweet_list = []
 pos_tags = []
 name_entities = []
 tags = []
 
-#Read
-for line in f:
-    i += 1
-    if (i % 5 == 1):
-        tweet_list.append(line)
-    if (i % 5 == 2):
-        pos_tags.append(line.strip().split(' '))
-    if (i % 5 == 3):
-        name_entities.append(line.strip().split(' '))
-    if (i % 5 == 0):
-        tags.append(line.strip().split(' '))
+#Read All Files
+for file_name in file_names:
+    #Open
+    f = open ("./data/" + file_name, 'r', encoding="UTF-8")
+    i = 0
 
-#Close
-f.close()
+    #Append to array
+    for line in f:
+        i += 1
+        if (i % 5 == 1):
+            tweet_list.append(line)
+        if (i % 5 == 2):
+            pos_tags.append(line.strip().split(' '))
+        if (i % 5 == 3):
+            name_entities.append(line.strip().split(' '))
+        if (i % 5 == 0):
+            tags.append(line.strip().split(' '))
+
+    #Close
+    f.close()
         
 i = 0
 x_train = []
 y_train = []
 
+#Make into features
 for sentence in tweet_list:
     word_tokens = sentence.strip().split(' ')
     j = 0
@@ -43,7 +47,8 @@ for sentence in tweet_list:
     i+=1
     x_train.append(x_train_sentence)
     y_train.append(y_train_sentence)
-    
+
+#Init Classifier and Train
 crf = sklearn_crfsuite.CRF(
     algorithm='lbfgs',
     c1=0.1,
@@ -54,8 +59,9 @@ crf = sklearn_crfsuite.CRF(
 crf.fit(x_train,y_train)
 labels = list(crf.classes_)
 print(labels)
+print(len(x_train))
 
 #Data Test
-#y_pred = crf.predict(x_train)
-#print(sklearn_crfsuite.metrics.flat_f1_score(y_train, y_pred,
-#                      average='weighted', labels=labels))
+y_pred = crf.predict(x_train)
+print(sklearn_crfsuite.metrics.flat_f1_score(y_train, y_pred,
+                      average='weighted', labels=labels))
