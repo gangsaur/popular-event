512e12758ebe61fad912f529077cb08c352101cc
gangsaur <andrihardono@yahoo.com>
2017-11-23 20:21:07 +0700

    Fix data + Fix train

--- train.py
+++ train.py
@@ -1,15 +1,10 @@
-from pystruct.models import GraphCRF
-from pystruct.learners import NSlackSSVM
-from time import time
 import numpy as np
-from sklearn.datasets import load_iris
+from time import time
 import sklearn_crfsuite
 import sklearn_crfsuite.metrics
-from pystruct.models import GraphCRF
-from pystruct.learners import NSlackSSVM
 
-file_name = "detik.txt"
-f = open (file_name, 'r')
+file_name = "kompas.txt"
+f = open ("./data/" + file_name, 'r', encoding="UTF-8")
 i = 0
 
 tweet_list = []
@@ -17,17 +12,21 @@ pos_tags = []
 name_entities = []
 tags = []
 
+#Read
 for line in f:
     i += 1
     if (i % 5 == 1):
         tweet_list.append(line)
     if (i % 5 == 2):
-        pos_tags.append(line.split(' '))
+        pos_tags.append(line.strip().split(' '))
     if (i % 5 == 3):
-        name_entities.append(line.split(' '))
+        name_entities.append(line.strip().split(' '))
     if (i % 5 == 0):
-        tags.append(line.split(' '))
+        tags.append(line.strip().split(' '))
 
+#Close
+f.close()
+        
 i = 0
 x_train = []
 y_train = []
@@ -38,13 +37,13 @@ for sentence in tweet_list:
     x_train_sentence = []
     y_train_sentence = []
     for token in word_tokens:
-        x_train_sentence.append({token,pos_tags[i][j],name_entities[i][j]})
+        x_train_sentence.append([token,pos_tags[i][j],name_entities[i][j]])
         y_train_sentence.append(tags[i][j])
         j+=1
     i+=1
     x_train.append(x_train_sentence)
     y_train.append(y_train_sentence)
-
+    
 crf = sklearn_crfsuite.CRF(
     algorithm='lbfgs',
     c1=0.1,
@@ -56,9 +55,7 @@ crf.fit(x_train,y_train)
 labels = list(crf.classes_)
 print(labels)
 
-
 #Data Test
-# y_pred = crf.predict(x_train)
-#
-# print(sklearn_crfsuite.metrics.flat_f1_score(y_train, y_pred,
-#                       average='weighted', labels=labels))
+#y_pred = crf.predict(x_train)
+#print(sklearn_crfsuite.metrics.flat_f1_score(y_train, y_pred,
+#                      average='weighted', labels=labels))
